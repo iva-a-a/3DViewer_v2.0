e8f8082af0691eb3d1a09c21b9e5228407f2e91b
satvajos <satvajos@student.21-school.ru>
2024-12-06 14:07:01 +0300

    rewrite vertex and edge

---
+++ src/model/vertex_edge.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <QVector3D>
+
+namespace s21 {
+
+class Vertex : public QVector3D {
+public:
+  Vertex() : QVector3D() {}
+  Vertex(float x, float y, float z) : QVector3D(x, y, z) {}
+  Vertex(const QVector3D &v) : QVector3D(v) {}
+  ~Vertex() = default;
+};
+
+class Edge {
+private:
+  Vertex &_begin;
+  Vertex &_end;
+
+public:
+  Edge(Vertex &begin, Vertex &end) : _begin(begin), _end(end) {}
+  ~Edge() = default;
+
+  const Vertex &getBeginPosition() const { return _begin; }
+
+  const Vertex &getEndPosition() const { return _end; }
+};
+
+} // namespace s21
