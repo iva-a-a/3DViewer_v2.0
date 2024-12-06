6925750a4bb03153c7f7a670bf662f00cf0f1b82
satvajos <satvajos@student.21-school.ru>
2024-12-06 14:08:20 +0300

    rewrite draw model

--- src/view/paintmodel.cpp
+++ src/view/paintmodel.cpp
@@ -6,28 +6,27 @@ void PaintModel::setModel(Figure &m) { model = &m; }
 
 void PaintModel::paintEvent(QPaintEvent *event) {
   QPainter painter(this);
-  painter.setRenderHint(QPainter::Antialiasing); // сглаживание
+  painter.setRenderHint(QPainter::Antialiasing); // Enable anti-aliasing
 
-  // вершины
+  // Draw vertices
   QPen pen(Qt::red, 5);
   painter.setPen(pen);
-  for (const QVector3D &v : model->getVertices()) {
+
+  for (const Vertex &v : model->getVertices()) {
     QPointF point(v.x() * 100 + width() / 2, -v.y() * 100 + height() / 2);
     painter.drawPoint(point);
   }
-  // грани
+
+  // Draw edges
   pen.setColor(Qt::black);
   pen.setWidth(1);
   painter.setPen(pen);
-  for (const QVector<int> &f : model->getFacets()) {
-    for (int i = 0; i < f.size(); ++i) {
-      const QVector3D &start = model->getVertices()[f[i]];
-      const QVector3D &end = model->getVertices()[f[(i + 1) % f.size()]];
-      QPointF startPoint(start.x() * 100 + width() / 2,
-                         -start.y() * 100 + height() / 2);
-      QPointF endPoint(end.x() * 100 + width() / 2,
-                       -end.y() * 100 + height() / 2);
-      painter.drawLine(startPoint, endPoint);
-    }
+
+  for (const Edge &edge : model->getFacets()) {
+    const QPointF startPoint(edge.getBeginPosition().x() * 100 + width() / 2,
+                             -edge.getBeginPosition().y() * 100 + height() / 2);
+    const QPointF endPoint(edge.getEndPosition().x() * 100 + width() / 2,
+                           -edge.getEndPosition().y() * 100 + height() / 2);
+    painter.drawLine(startPoint, endPoint);
   }
 }
