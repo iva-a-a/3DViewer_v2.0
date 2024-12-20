dd8c98b956495f0571e14c423c274276b417b38f
satvajos <satvajos@student.21-school.ru>
2024-12-20 13:28:41 +0300

    add param for saving settings

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -3,15 +3,24 @@
 #include "../model/figure.h"
 
 namespace s21 {
+struct Parameters {
+  float scale = 1.0f;
+  float shift_x = 0.0f;
+  float shift_y = 0.0f;
+  float shift_z = 0.0f;
+  float rotate_x = 0.0f;
+  float rotate_y = 0.0f;
+  float rotate_z = 0.0f;
+};
 
 class Facade {
 private:
   Figure model;
-
+  Parameters p;
   float degreesInRadians(float x);
 
 public:
-  Facade(const QString &filename) : model(Figure(filename)) {}
+  Facade(const QString &filename) : model(Figure(filename)) { p = {}; }
   ~Facade() = default;
 
   void moveFigure(float x, float y, float z);
